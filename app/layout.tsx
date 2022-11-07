d482aaba2b78ee19daf15e56909aa1f92d207ae2
박혜민 <hyemin@ktown4u.com>
2022-11-07 21:19:22 +0900

    폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.css";
 import type { ReactNode } from "react";
+import localFont from "@next/font/local";
+
+const suitFont = localFont({ src: "./my-font.woff2" });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={suitFont.className}>
       <head>
         <title>햄햄위키</title>
         <meta name="description" content="Ham's dev wiki" />
